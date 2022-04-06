9db6a3be3b92ac66a932f3d95321b8f89482e48e
yumiheduan <yumiheduan@gmail.com>
2022-04-06 11:02:16 +0900

    kana_nameをひらがなスペース無しに修正

--- database/factories/MemberFactory.php
+++ database/factories/MemberFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Member::class, function (Faker $faker) {
     return [
-        'kana_name' => $faker->name,
+        'kana_name' => mb_convert_kana(str_replace(' ','',$faker->kanaName), 'c'),
         'phone'=> $faker->phoneNumber,
         'email' => $faker->safeEmail,
     ];
