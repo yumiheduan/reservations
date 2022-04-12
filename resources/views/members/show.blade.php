790648afa175ad0d40ba90ac22fb4a89ae2c2e98
yumiheduan <yumiheduan@gmail.com>
2022-04-12 21:35:32 +0900

    timeテーブル一覧へのリンク追加

--- resources/views/members/show.blade.php
+++ resources/views/members/show.blade.php
@@ -6,7 +6,7 @@
     <div class="collapse navbar-collapse" id="Navbar">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="">予約状況</a>
+                <a class="nav-link" aria-current="page" href="{{ route('times.index') }}">予約状況</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link active" href="">会員情報</a>
