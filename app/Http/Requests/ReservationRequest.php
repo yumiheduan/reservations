016171426580e0024cec8a7a91b17b4f8c17d166
yumiheduan <yumiheduan@gmail.com>
2022-04-13 21:38:52 +0900

    営業時間外の予約登録の防止バリデーション

--- app/Http/Requests/ReservationRequest.php
+++ app/Http/Requests/ReservationRequest.php
@@ -25,7 +25,7 @@ class ReservationRequest extends FormRequest
     public function rules()
     {
         /**
-         * 予約重複の防止バリデーション
+         * 予約重複登録の防止バリデーション
          *   $attribute: 検証中の属性名
          *   $value    : 検証中の属性の値
          *   $fail     : 失敗時に呼び出すメソッド?
@@ -41,11 +41,25 @@ class ReservationRequest extends FormRequest
                 $fail('その時間はすでに予約が入っています。'); // エラーメッセージ
             }
         };
+        /**
+         * 24時以降予約登録の防止バリデーション（営業時間が24時までの為）
+         *   $attribute: 検証中の属性名
+         *   $value    : 検証中の属性の値
+         *   $fail     : 失敗時に呼び出すメソッド?
+         **/
+        $validate_time = function ($attribute, $value, $fail) {
 
+            // 入力されたデータが24時を超えていたら失敗にする。
+            if ($this->start_time + $this->use_time >= 24)
+            {
+                $fail('営業時間は24時までです。'); // エラーメッセージ
+            }
+        };
+        
         return [
             'reservation_date' => ['required', 'date', 'after_or_equal:today'],
             'room_id' => ['required', 'integer'],
-            'start_time' => ['required', 'integer', $validate_func],
+            'start_time' => ['required', 'integer', $validate_func, $validate_time],
             'use_time' => ['required', 'integer'],
 
         ];
