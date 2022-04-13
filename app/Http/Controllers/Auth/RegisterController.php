2f22feb1751836cff2f4d5e0c002fae3f335e4b6
yumiheduan <yumiheduan@gmail.com>
2022-04-13 14:36:48 +0900

    ログイン後とユーザー登録後の遷移先をtimesに変更

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
      * @var string
      */
     // protected $redirectTo = RouteServiceProvider::HOME;
-    protected $redirectTo = 'tims';
+    protected $redirectTo = 'times';
 
     /**
      * Create a new controller instance.
