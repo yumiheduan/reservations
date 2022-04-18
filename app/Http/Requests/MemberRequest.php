d1adca206d75983d423772f19d47bc810de0f524
yumiheduan <yumiheduan@gmail.com>
2022-04-18 15:28:16 +0900

    電話番号のバリデーション修正

--- app/Http/Requests/MemberRequest.php
+++ app/Http/Requests/MemberRequest.php
@@ -27,7 +27,7 @@ class MemberRequest extends FormRequest
         return [
             'kana_name' =>  'required|max:100|regex:/^[ぁ-んー]+$/',
             'phone' => [
-                'regex:/^[0-9]+$/',
+                'regex:/^[0-9-]+$/',
                 // membersテーブルでユニーク制約。ignoreで入力されたidはバリデーションから除外する
                 Rule::unique('members')->ignore($this->id)->where(function ($query) {
                     // 入力されたkana_nameの値と同じ値を持つレコードでのみ検証する
@@ -48,7 +48,7 @@ class MemberRequest extends FormRequest
             'kana_name.required' => '氏名かなを入力してください。',
             'kana_name.max:100' => '氏名かなは100文字以下にしてください。',
             'kana_name.regex'=> '氏名かなはひらがなだけで入力してください。',
-            'phone.regex' => '電話番号はハイフンなしの半角数字で入力してください。',
+            'phone.regex' => '電話番号は半角数字とハイフンで入力してください。',
             'phone.unique' => 'このメンバーはすでに登録されています。',
             'email.email' => 'Emailは255文字以下にしてください。',
         ];
