e79c8139921e18d46d3c5487bb841c1c7093e7a9
yumiheduan <yumiheduan@gmail.com>
2022-04-27 12:05:59 +0900

    layouts修正

--- resources/views/layouts/reservations.blade.php
+++ resources/views/layouts/reservations.blade.php
@@ -28,7 +28,7 @@
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
                                 <a class="nav-link" aria-current="page"
-                                    href="{{ route('times.index') }}">予約状況</a>
+                                    href="{{ route('times.index') }}">本日の予約状況</a>
                             </li>
                         </ul>
                         <form class="d-flex" action="{{ route('members.search') }}" method="get">
